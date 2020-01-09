fb024aea29cdbdd43dab9c23a4c5e668c63a1337
AlastairM-E <alastairunityemail@gmail.com>
2020-01-09 22:35:22 +0000

    also removed vendor.js as useless

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry : {
         main : './src/index.js',
-        vendor : './src/vendor.js',
+        // vendor : './src/vendor.js',
     },
     plugins : [
         new HtmlWebpackPlugin({ template : './src/template.html', }),
